b5bfadefc4de42c160a33a0fe26055b7a545610d
Juan Ignacio Chiappa <chiappajuanignacio@gmail.com>
2026-08-28 17:01:10 -0300

    Refactoriza ConexionBD a clase estática y usa config
    Se ha convertido ConexionBD en una clase estática, utilizando System.Configuration y Microsoft.Data.SqlClient. La cadena de conexión ahora se obtiene desde el archivo de configuración mediante ConfigurationManager. Se añade el método estático ObtenerConexion(). Se agregan referencias a los paquetes NuGet necesarios en StayEasy.DAL.csproj.

--- StayEasy.DAL/Infraestructura/ConexionBD.cs
+++ StayEasy.DAL/Infraestructura/ConexionBD.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Configuration;
+using Microsoft.Data.SqlClient;
 
 namespace StayEasy.DAL.Infraestructura
 {
-    internal class ConexionBD
+    internal static class ConexionBD
     {
+        private static string _cadenaConexion => ConfigurationManager.ConnectionStrings["StayEasyDB"].ConnectionString;
+
+        public static SqlConnection ObtenerConexion()
+        {
+            return new SqlConnection(_cadenaConexion);
+        }
     }
 }
