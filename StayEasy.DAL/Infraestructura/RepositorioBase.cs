1731592e47b547782cb9568918991b48b18b784f
Juan Ignacio Chiappa <chiappajuanignacio@gmail.com>
2026-08-28 17:06:22 -0300

    Añadir clase abstracta RepositorioBase con IDisposable
    Se crea la clase abstracta RepositorioBase en StayEasy.DAL.Infraestructura. Implementa IDisposable y gestiona una conexión SqlConnection, inicializándola en el constructor y liberando recursos en Dispose.

---
+++ StayEasy.DAL/Infraestructura/RepositorioBase.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StayEasy.DAL.Infraestructura
+{
+    public abstract class RepositorioBase : IDisposable
+    {
+        protected SqlConnection _conexion;
+
+        protected RepositorioBase()
+        {
+            _conexion = ConexionBD.ObtenerConexion();
+        }
+
+        public void Dispose()
+        {
+            if (_conexion != null)
+            {
+                if (_conexion.State == System.Data.ConnectionState.Open)
+                {
+                    _conexion.Close();
+                }
+                _conexion.Dispose();
+            }
+        }
+    }
+}
