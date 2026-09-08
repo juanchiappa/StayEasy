a2a17a36a25540307a47931d8aee81831bb2ad22
lean-04 <leandrogonzalezt2018@gmail.com>
2026-09-07 22:09:42 -0300

    reserva xaml

--- StayEasy.UI/Dashboard.xaml.cs
+++ StayEasy.UI/Dashboard.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using StayEasy.BLL;
+using StayEasy.Seguridad;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -9,7 +10,19 @@ namespace StayEasy.UI
         public Dashboard()
         {
             InitializeComponent();
-            
+            Lbl_Titulo.Text = GestorSesion.Instancia.UsuarioLogueado.NombreUsuario;
+            Lbl_UsuarioNombre.Text = GestorSesion.Instancia.UsuarioLogueado.NombreUsuario;
+            string[] NombreCompleto = GestorSesion.Instancia.UsuarioLogueado.NombreCompleto.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            string iniciales = "";
+
+            // Verificar que haya al menos dos palabras
+            if (NombreCompleto.Length >= 2)
+            {
+                // Tomar la primera letra de la primera palabra y la primera de la segunda
+                iniciales = $"{NombreCompleto[0][0]}{NombreCompleto[1][0]}".ToUpper();
+            }
+            Lbl_iniciales.Text = iniciales;
         }
     }
 }
